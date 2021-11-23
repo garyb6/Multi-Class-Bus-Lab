ff308b812a52e4cd3063259802768ad1afee85fd
Clau <clau.rjimenez@gmail.com>
2021-11-23 16:34:20 +0000

    fixed pick_up_from_stop. Added got_on empty list to temporarily store Person instances to avoid unexpected behaviour while removing items from a list while still looping through it

--- src/bus.py
+++ src/bus.py
@@ -1,3 +1,6 @@
+from src.person import Person
+
+
 class Bus:
     def __init__(self, route_number, destination, price, capacity):
         self.route_number = route_number
@@ -23,14 +26,22 @@ class Bus:
         self.passengers.clear()
 
     def pick_up_from_stop(self, bus_stop):
+        got_on = []
+
         for passenger in bus_stop.queue:
             if self.capacity < 1:
                 return
+            if passenger.cash < self.price:
+                return
 
             if self.destination == passenger.destination:
+                passenger.pay_fare(self)
                 self.pick_up(passenger)
                 self.capacity -= 1
-                bus_stop.queue.remove(passenger)
+                got_on.append(passenger)
+
+        for passenger in got_on:
+            bus_stop.queue.remove(passenger)
 
         # self.passengers.extend(bus_stop.queue)
         # bus_stop.clear()
