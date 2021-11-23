12b086b6137ed34e953bf5305ae89b1e63209650
Clau <clau.rjimenez@gmail.com>
2021-11-23 14:08:58 +0000

    added Person and Bus clases and finished MVP

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,22 @@
+class Bus:
+    def __init__(self, route_number, destination, price, capacity):
+        self.route_number = route_number
+        self.capacity = capacity
+        self.destination = destination
+        self.price = price
+        self.passengers = []
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, passenger):
+        self.passengers.append(passenger)
+
+    def drop_off(self, passenger):
+        self.passengers.remove(passenger)
+
+    def empty(self):
+        self.passengers.clear()
